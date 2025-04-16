a04e6878033a650a3b4ba17724ede1373c7da45a
isra omera <161392661+IsraOmera@users.noreply.github.com>
2025-04-16 20:37:55 +0200

    Update image_processing.py

--- src/image_processing.py
+++ src/image_processing.py
@@ -4,13 +4,13 @@ import re
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-def extract_text(image_path: str) -> str:
+def read_image(image_path: str) -> str:
     image = Image.open(image_path)
     text = pytesseract.image_to_string(image)
     return text
 
-def read_image(file_path):
-    text = extract_text(file_path)
+def clean_extracted_text_img(file_path):
+    text = read_image(file_path)
     cleaned_text = re.sub(r"[\[\]{}(),:\*]", "",text) 
     
     return cleaned_text
